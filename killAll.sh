7813e1aa584c4649a3af0ed87a0bbc8e089cf63c
Amit Shrivastava <amit.shrivastava@talentica.com>
2015-10-02 11:11:16 +0530

    Added check for already running process.

--- killAll.sh
+++ killAll.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-for line in `cat services.pid`
-do 
-echo "Killing process with PID :"$line
-kill -9 $line &
-done
+echo "Stopping customer management service with ID : `cat customer_management_artifact/PID`"
 
-rm -rf services.pid
+kill -9 `cat customer_management_artifact/PID`
+
+rm -rf customer_management_artifact/PID
+
+echo "Stopping inventory management service with ID : `cat inventory_management_artifact/PID`"
+
+kill -9 `cat inventory_management_artifact/PID`
+
+rm -rf inventory_management_artifact/PID
+
+echo "Stopping order management service with ID : `cat order_management_artifact/PID`"
+
+kill -9 `cat order_management_artifact/PID`
+
+rm -rf order_management_artifact/PID
+
+echo "Stopping shipment management service with ID : `cat shipment_management_artifact/PID`"
+
+kill -9 `cat shipment_management_artifact/PID`
+
+rm -rf shipment_management_artifact/PID
--- runAll.sh
+++ runAll.sh
@@ -11,35 +11,58 @@
 
 echo "Do you want to start customer management service : y/n" 
 read custom_service_input
+
 if [ $custom_service_input == "y" ]; then
-	echo "Starting customer management service"
-	java -jar ./customer_management_artifact/target/customer-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1101 -Dcom.sun.management.jmxremote.authenticate=false > customer_management.log 2>&1 &
-	echo $! >> services.pid
+	if [ -f customer_management_artifact/PID ]; then
+		echo "Process customer management service is already running"
+	else
+		echo "Starting customer management service"
+		rm -rf customer_management_artifact/PID
+		java -jar ./customer_management_artifact/target/customer-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1101 -Dcom.sun.management.jmxremote.authenticate=false > customer_management.log 2>&1 &
+		echo $! > customer_management_artifact/PID
+	fi
 fi
 
 echo  "Do you want to start inventory management service : y/n" 
 read inventory_service_input
+
 if [ $inventory_service_input == "y" ]; then
-	echo "Starting inventory management service"
-	java -jar ./inventory_management_artifact/target/inventory-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1102 -Dcom.sun.management.jmxremote.authenticate=false > inventory_management.log 2>&1 &
-	echo $! >> services.pid
+	if [ -f inventory_management_artifact/PID ]; then
+		echo "Process inventory management service is already running"
+	else
+		echo "Starting inventory management service"
+		rm -rf inventory_management_artifact/PID
+		java -jar ./inventory_management_artifact/target/inventory-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1102 -Dcom.sun.management.jmxremote.authenticate=false > inventory_management.log 2>&1 &
+		echo $! > inventory_management_artifact/PID
+	fi
 fi
 
 echo  "Do you want to start order management service: y/n" 
 read order_service_input
+
 if [ $order_service_input == "y" ]; then
-	echo "Starting order management service"
-	java -jar ./order_management_artifact/target/order-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1103 -Dcom.sun.management.jmxremote.authenticate=false > order_management.log 2>&1 &
-	echo $! >> services.pid
-	
+	if [ -f order_management_artifact/PID ]; then
+		echo "Process order management service is already running"
+	else
+		echo "Starting order management service"
+		rm -rf order_management_artifact/PID
+		java -jar ./order_management_artifact/target/inventory-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1102 -Dcom.sun.management.jmxremote.authenticate=false > inventory_management.log 2>&1 &
+		echo $! > order_management_artifact/PID
+	fi	
 fi
 
 echo  "Do you want to start shipment management service: y/n"
 read shipment_service_input
+
 if [ $shipment_service_input == "y" ]; then
-	echo "Starting shipment management service"
-	java -jar ./shipment_management_artifact/target/shipment-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1103 -Dcom.sun.management.jmxremote.authenticate=false > shipment_management.log 2>&1 &
-	echo $! >> services.pid
+	if [ -f shipment_management_artifact/PID ]; then
+		echo "Process shipment management service is already running"
+	else
+		echo "Starting shipment management service"
+		rm -rf shipment_management_artifact/PID
+		java -jar ./shipment_management_artifact/target/inventory-management-1.2.6.RELEASE.jar -Dcom.sun.management.jmxremote.port=1102 -Dcom.sun.management.jmxremote.authenticate=false > inventory_management.log 2>&1 &
+		echo $! > shipment_management_artifact/PID
+	fi	
 fi
 
 
